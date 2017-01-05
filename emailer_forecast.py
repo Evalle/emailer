d5845a1c9a39e59a064d74dbf4ffa19171ad8735
evalle <shmarnev@gmail.com>
2017-01-05 10:53:38 +0100

    Create separate module for sending emails functionality

--- emailer_forecast.py
+++ emailer_forecast.py
@@ -1,8 +1,8 @@
 import sys
 import os
-import smtplib
 from configparser import SafeConfigParser
 from weather import get_weather_forecast
+from sender import send_emails
 
 def get_emails():   
     emails = dict()
@@ -60,24 +60,6 @@ def get_smtp_config():
     else: 
         print('Make sure you have smtp.config file in working directory!')
         sys.exit(1)
-
-
-def send_emails(emails, forecast, username, password):
-    # Connect to smtp server 
-    server = smtplib.SMTP('smtp.gmail.com', '587')
-    # Start encryption
-    server.starttls()
-    # Login
-    server.login(username, password)
-
-    # Send to whole mail list
-    for to_email, name in emails.items():
-        message = 'Subject: Weather forecast for today\n'
-        message += 'Hi ' + name + '!\n\n'
-        message += forecast + '\n\n'
-        message += 'Cheers, your email forecast app'
-        server.sendmail('emailer.app11@gmail.com', to_email, message)
-    server.quit()
 
  
 def main():
---
+++ sender.py
@@ -0,0 +1,18 @@
+import smtplib
+
+def send_emails(emails, forecast, username, password):
+    # Connect to smtp server 
+    server = smtplib.SMTP('smtp.gmail.com', '587')
+    # Start encryption
+    server.starttls()
+    # Login
+    server.login(username, password)
+
+    # Send to whole mail list
+    for to_email, name in emails.items():
+        message = 'Subject: Weather forecast for today\n'
+        message += 'Hi ' + name + '!\n\n'
+        message += forecast + '\n\n'
+        message += 'Cheers, your email forecast app'
+        server.sendmail('emailer.app11@gmail.com', to_email, message)
+    server.quit()
